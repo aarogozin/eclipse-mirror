c5ee04e8b1a40d1590cc67b014fb4def973032b6
aarogozin <anton.rogozin@csssr.com>
2020-02-08 12:08:00 +0400

    fix

--- eclipse-mirror.groovy
+++ eclipse-mirror.groovy
@@ -1,12 +1,12 @@
 pipeline { 
     environment {
-        DEST_SERVER = credentials('server16-ip')
-        DEST_PASSWORD = credentials('server16-pass')
-        ECLIPSE_DEST = $WORKSPACE/eclipse/eclipse
+        eclipseLocation = $WORKSPACE/eclipse/eclipse
+        dest = file:///$WORKSPACE/tmp/$repoName/
+
     }
     parameters {
     string(name: 'SOURCE', defaultValue: '--//--//--//--', description: 'Eclipse repository url')
-    string(name: 'DEST', defaultValue: 'release', description: 'Desirable path of mirror after URL')
+    string(name: 'repoName', defaultValue: 'release', description: 'Desirable path of mirror after URL')
     }
     stages {
         stage('download and extract eclipse') {
@@ -16,14 +16,22 @@ pipeline {
               rm -rf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz
             }
         }
-        stage('mirror') {
+        stage('download repository') {
             environment {
                 DEST_SERVER = credentials('server16-ip')
             }
-            steps {
-                
-                // 
+            steps ('download mirror') {
+                $eclipseLocation -nosplash -verbose -application org.eclipse.equinox.p2.metadata.repository.mirrorApplication -source $SOURCE -destination $dest
+                $eclipseLocation -nosplash -verbose -application org.eclipse.equinox.p2.artifact.repository.mirrorApplication -source $SOURCE -destination $dest
             }
+        }
+        stage ('push repository') {
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'server16', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) 
+            environment {
+                DEST_SERVER = credentials('server16-ip')
+            }
+            ssh-keyscan $server >> ~/.ssh/known_hosts
+            sshpass -p $PASSWORD rsync -r  $WORKSPACE/tmp/* $USERNAME@$DEST_SERVER:/data/update-sites/mirrors
         }
     }
 }
