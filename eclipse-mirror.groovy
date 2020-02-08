adff6b75489c5fa109a9be75c84ee8f070a9ecb2
aarogozin <anton.rogozin@csssr.com>
2020-02-08 12:27:22 +0400

    1

--- eclipse-mirror.groovy
+++ eclipse-mirror.groovy
@@ -13,8 +13,8 @@ pipeline {
     stages {
         stage('download and extract eclipse') {
             steps {
-              sh wget https://download.springsource.com/release/ECLIPSE/2019-09/eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz && \
-              tar xf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz && \
+              wget https://download.springsource.com/release/ECLIPSE/2019-09/eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz 
+              tar xf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz 
               rm -rf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz
             }
         }
