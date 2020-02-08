48d3c33e9df63ed6dbdc80cc6f8594f28acf3eb2
aarogozin <anton.rogozin@csssr.com>
2020-02-08 13:07:12 +0400

    6

--- eclipse-mirror.groovy
+++ eclipse-mirror.groovy
@@ -11,14 +11,14 @@ pipeline {
     string(name: 'repoName', defaultValue: 'release', description: 'Desirable path of mirror after URL')
     }
     stages {
-        stage('download and extract eclipse') {
-            steps {
-              sh """ 
-              wget https://download.springsource.com/release/ECLIPSE/2019-09/eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz 
-              tar xf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz 
-              rm -rf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz
-              """
-            }
+        // stage('download and extract eclipse') {
+        //     steps {
+        //       sh """ 
+        //       wget https://download.springsource.com/release/ECLIPSE/2019-09/eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz 
+        //       tar xf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz 
+        //       rm -rf eclipse-java-2019-09-R-linux-gtk-x86_64.tar.gz
+        //       """
+        //     }
         }
         stage('download repository') {
             steps ('download mirror') {
@@ -37,7 +37,9 @@ pipeline {
             }
             steps ('Upload repository to server') {
                 script {
-                withCredentials([[credentialsId:'server16', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) 
+                withCredentials([
+                usernamePassword(credentialsId: "server16", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')
+              ]) 
                     sh """
                     ssh-keyscan $server >> ~/.ssh/known_hosts
                     sshpass -p $PASSWORD rsync -r  $WORKSPACE/tmp/* $USERNAME@$DEST_SERVER:/data/update-sites/mirrors
